31cca752aec92d45a049a163bd3e4d0833d4238b
Inesa Kostova <kostovainesa@gmail.com>
2025-03-05 14:12:43 +0200

    Add files via upload

---
+++ Old Book.cs
@@ -0,0 +1,22 @@
+﻿string nameBook = Console.ReadLine();
+int countBooks = 0;
+string currentBook =Console.ReadLine();
+
+while (nameBook != "No More Books")
+{
+    if (currentBook == nameBook)
+    {
+        Console.WriteLine($"You checked {countBooks} books and found it.");
+        break;
+    }
+
+    countBooks++;
+    currentBook = Console.ReadLine();
+
+    if (currentBook == "No More Books")
+    {
+        Console.WriteLine("The book you search is not here!");
+        Console.WriteLine($"You checked {countBooks} books.");
+        break;
+    }
+}
