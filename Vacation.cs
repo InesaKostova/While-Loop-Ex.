8b3a0c6ad942f3d7503c6f10ed0ae406e10e462d
Inesa Kostova <kostovainesa@gmail.com>
2025-03-07 14:20:30 +0200

    Add files via upload

---
+++ Vacation.cs
@@ -0,0 +1,31 @@
+﻿double needMoney = double.Parse(Console.ReadLine());
+double saveMoney = double.Parse(Console.ReadLine());
+string action = "";
+double currentSum = 0;
+int days = 0;
+
+while(true)
+{action = Console.ReadLine();
+    currentSum = double.Parse(Console.ReadLine());
+    days++;
+    if(days>=5 && action=="spend")
+    {
+        Console.WriteLine("You can't save the money.");
+        Console.WriteLine($"{days}");
+        break;
+
+    }
+    else if(action=="save")
+    {
+        currentSum++;
+        days++;
+        if(currentSum>=needMoney)
+        {
+            Console.WriteLine($"You saved the money for {days} days.");
+            break;
+        }
+
+    }
+
+
+}
