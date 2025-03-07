81866306fbc051c6e1c8bb5dd4db1d21b84a3409
Inesa Kostova <kostovainesa@gmail.com>
2025-03-07 15:41:38 +0200

    Add files via upload

---
+++ Cake.cs
@@ -0,0 +1,9 @@
+﻿int width = int.Parse(Console.ReadLine());
+int lenght = int.Parse(Console.ReadLine());
+int size = width * lenght;
+
+string input=Console.ReadLine();
+while(input!="STOP")
+{
+
+}
