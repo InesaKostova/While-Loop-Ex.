e71f2e6095e050f6080553dd234c8837e40e2ebf
Inesa Kostova <kostovainesa@gmail.com>
2025-03-08 14:11:07 +0200

    Add files via upload

---
+++ Exam Preparation.cs
@@ -0,0 +1,31 @@
+﻿int badGrade = int.Parse(Console.ReadLine());
+double gradeSum = 0;
+int gradeCount = 0;
+int badGradeCount = 0;
+string lastName = "";
+
+while (true)
+{
+    string name=Console.ReadLine();
+    if(name =="Enough")
+    {
+        Console.WriteLine($"Average score: {gradeSum / gradeCount:F2}");
+        Console.WriteLine($"Number of problems: {gradeCount}");
+        Console.WriteLine($"Last problem: {lastName}");
+        break;
+    }
+    int currentGrade = int.Parse(Console.ReadLine());
+    if(currentGrade<=4)
+    {
+        badGradeCount++;
+        if(badGradeCount==badGrade)
+        {
+            Console.WriteLine($"You need a break, {badGrade} poor grades.");
+            break;
+        }
+
+    }
+    gradeCount++;
+    gradeSum+= currentGrade;
+    lastName = name;
+}
