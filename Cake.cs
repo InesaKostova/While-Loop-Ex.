31c9cc617a1ed3e72f1b3801dba02764c2894c94
Inesa Kostova <kostovainesa@gmail.com>
2025-03-08 14:15:51 +0200

    Add files via upload

--- Cake.cs
+++ Cake.cs
@@ -2,8 +2,19 @@
 int lenght = int.Parse(Console.ReadLine());
 int size = width * lenght;
 
-string input=Console.ReadLine();
-while(input!="STOP")
+string input = Console.ReadLine();
+while (input != "STOP")
 {
-
+    size -= int.Parse(input);
+    if (size <= 0)
+    {
+        Console.WriteLine($"No more cake left! You need {Math.Abs(size)} pieces more.");
+        break;
+    }
+    input = Console.ReadLine();
 }
+if (input == "STOP")
+{
+    Console.WriteLine($"{size} pieces are left.");
+}
+
