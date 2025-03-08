94db111b3c1e96a298b7204444f9ecc882005eec
Inesa Kostova <kostovainesa@gmail.com>
2025-03-08 14:17:18 +0200

    Add files via upload

--- Moving.cs
+++ Moving.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int width = int.Parse(Console.ReadLine());
+int lenght = int.Parse(Console.ReadLine());
+int height = int.Parse(Console.ReadLine());
+
+int volume = width * height * lenght;
+
+string input = Console.ReadLine();
+while (input != "Done")
+{
+    volume -= int.Parse(input);
+    if (volume < 0)
+    {
+        Console.WriteLine($"No more free space! You need {Math.Abs(volume)} Cubic meters more.");
+        break;
+    }
+    input = Console.ReadLine();
+}
+if (input == "Done")
+{
+    Console.WriteLine($"{volume} Cubic meters left.");
+}
