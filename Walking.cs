bbdbd8e29991b707cdbf916507161d37e1319d1c
Inesa Kostova <kostovainesa@gmail.com>
2025-03-09 08:41:43 +0200

    Add files via upload

---
+++ Walking.cs
@@ -0,0 +1,32 @@
+﻿int goal = 10000;
+int totalSteps = 0;
+while (totalSteps < goal)
+        {
+            string input = Console.ReadLine();
+
+            if (input == "Going home")
+            {
+                int stepsHome = int.Parse(Console.ReadLine());
+                totalSteps += stepsHome;
+                break;
+            }
+            else
+            {
+                int steps = int.Parse(input);
+                totalSteps += steps;
+            }
+        }
+        if (totalSteps >= goal)
+        {
+            Console.WriteLine("Goal reached! Good job!");
+            if (totalSteps > goal)
+            {
+                Console.WriteLine($"{totalSteps - goal} steps over the goal!");
+            }
+        }
+        else
+        {
+            Console.WriteLine($"{goal - totalSteps} more steps to reach goal.");
+        }
+    
+
